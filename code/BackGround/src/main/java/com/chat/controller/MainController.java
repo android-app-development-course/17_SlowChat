307ce34a560c8dc1391ee11f95374c1b23f6a521
v <729164860@qq.com>
2017-12-28 18:36:20 +0800

    后台骨架已经搭好

---
+++ code/BackGround/src/main/java/com/chat/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/test.do",method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+}
