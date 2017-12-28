86caee0567f8bae1bf2a7973b7b768169b46e73f
v <729164860@qq.com>
2017-12-28 19:31:32 +0800

    给后台上了个丝袜哥

--- code/BackGround/src/main/java/com/chat/controller/MainController.java
+++ code/BackGround/src/main/java/com/chat/controller/MainController.java
@@ -3,11 +3,25 @@ package com.chat.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 public class MainController {
     @RequestMapping(value = "/test.do",method = RequestMethod.GET)
-    public String index(){
-        return "index";
+    @ResponseBody
+    public Object index(){
+        List<String> list=new ArrayList<String>();
+        list.add("123");
+        list.add("cs");
+        list.add("mywrod");
+
+        HashMap map=new HashMap();
+        map.put("ab",list);
+        map.put("cd","sa");
+        return map;
     }
 }
