4001e02bcb887e2e935472c05605e9a85c3f2fdd
v <729164860@qq.com>
2018-01-02 16:14:47 +0800

    完成了注册模块

--- code/BackGround/src/main/java/com/chat/controller/login/LoginController.java
+++ code/BackGround/src/main/java/com/chat/controller/login/LoginController.java
@@ -4,6 +4,7 @@ import com.chat.application.Response;
 import com.chat.dao.UserDao;
 import com.chat.entity.User;
 import com.chat.service.login.LoginService;
+import com.chat.service.login.RegisterService;
 import com.chat.util.HibernateUtil;
 import com.chat.util.SpringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,24 +23,44 @@ public class LoginController {
     private LoginService loginService;
     @Autowired
     private UserDao userDao;
+    @Autowired
+    private RegisterService registerService;
 
     @RequestMapping(value = "/login/login.do",method = RequestMethod.GET)
     @ResponseBody
     public Map<String,Object> login(HttpServletRequest request, HttpServletResponse response, String email, String pwd){
-        HibernateUtil.getCurrentSession().beginTransaction();
-        Map<String,Object> map;
-
         if(!loginService.isPass(email,pwd)){
-            map=Response.getFailureResponse("账号或密码不正确！");
+            return Response.getFailureResponse("账号或密码不正确！");
         }
-
-        map=Response.getsuccessResponse("登录成功！");
 
         //添加cookie
         User user= userDao.getUserByEmail(email);
         loginService.addCookie(request,response,user);
 
-        HibernateUtil.getCurrentSession().getTransaction().commit();
-        return map;
+        return Response.getsuccessResponse("登录成功！");
+    }
+
+    @RequestMapping(value = "/login/register.do",method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String,Object> register(String email,String pwd){
+        if(!registerService.isEmailFit(email)){
+            return Response.getFailureResponse("邮箱格式不符合要求");
+        }
+
+        if(!registerService.isEmailUnregistered(email)){
+            return Response.getFailureResponse("该邮箱已注册");
+        }
+
+        if(!registerService.isPwdFit(pwd)){
+            return Response.getFailureResponse("密码格式不符合要求");
+        }
+
+        //创建user并且把user添加到数据库里面
+        User user=SpringUtil.getBean(User.class);
+        user.setEmail(email);
+        user.setPwd(pwd);
+
+        userDao.add(user);
+        return Response.getsuccessResponse();
     }
 }
